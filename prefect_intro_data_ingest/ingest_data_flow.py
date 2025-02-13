868036edb54a2b14f3efb92ee91546da6146bf15
Koustubh2111 <koustubh.sudarshan@gmail.com>
2025-02-13 14:48:03 -0500

    convert data ingestion to workflow

--- prefect_intro_data_ingest/ingest_data_flow.py
+++ prefect_intro_data_ingest/ingest_data_flow.py
@@ -1,28 +1,55 @@
+#%%
 import pandas as pd
 from sqlalchemy import create_engine
 from time import time
 from prefect import flow, task
 
+#%%
+
+
+@task(log_prints = True)
+def extract_data(csv_file, chunksize = 1000):
+
+    return pd.read_csv(csv_file, iterator=True, \
+                      chunksize=chunksize)
+
+
+@task(log_prints = True)
+def transform_data(df):
+
+    #Combine three timestamp columns into one 
+    df = df.rename(columns={'arrival_date_day_of_month': 'day', 'arrival_date_month': 'month',\
+                             'arrival_date_year': 'year'})
+    df['month'] = pd.to_datetime(df['month'], format = "%B").dt.month
+    timestamp_cols = ['year', 'month', 'day']
+    df['arrival_timestamp'] = pd.to_datetime(df[timestamp_cols])
+    df.drop(timestamp_cols, axis = 1, inplace = True)
+
+    #Rearrange cols
+    timestamp = df.pop('arrival_timestamp')
+    df.insert(3, 'arrival_timestamp', timestamp)
+
+    #remove fields with days_in_waiitng_list < 50 and previous_bookings_not_cancelled > 50
+    #Arbitrary transformation
+    return df[(df['days_in_waiting_list'] < 50) & (df['previous_bookings_not_canceled'] > 50)].reset_index()
+
 @task(log_prints=True, retries=3)
-def ingest_date(user,password,host, port, db, table_name, csv_file):
+def load_data(user,password,host, port, db, table_name, df_iter):
 
     '''
-    Task used with metadata and upstream dependencies 
+    task decorator used for metadata and upstream dependencies 
     '''
 
-    df_iter = pd.read_csv(csv_file, iterator=True, \
-                      chunksize=10000)
-    
     #Create the SQL engine
     #Use postgresql in the URL and not postgres
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    engine = create_engine(postgres_url)
 
     #Add the header to the database
     df = next(df_iter)
     #Add to table with 'name' - take care to not use dashes in the name.
     df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    '''
 
     #Ingest each of the chunks into the sql table using append
     chunk = 0
@@ -45,7 +72,7 @@ def ingest_date(user,password,host, port, db, table_name, csv_file):
             print('End of file reached')
             break
             
-    '''
+    
 
 @flow(name='Ingest_flow')
 def main_flow():
@@ -56,8 +83,12 @@ def main_flow():
     db = 'hotel_bookings'
     table_name = 'hotel_bookings_data'
     csv_file = '../ingesting-data-postgres/hotel-bookings-data/hotel_bookings.csv'
+    chunksize = 10000
 
-    ingest_date(user,password,host, port, db, table_name, csv_file)
+    
+    raw_data = extract_data(csv_file=csv_file, chunksize=chunksize)
+    transformed_data = transform_data(raw_data)
+    load_data(user,password,host, port, db, table_name, transformed_data)
 
 
 if __name__ == '__main__':
