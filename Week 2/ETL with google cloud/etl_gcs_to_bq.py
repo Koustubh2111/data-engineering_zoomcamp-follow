6bda0057b4446dda74c6fd1162c8899e4a102433
Koustubh2111 <koustubh.sudarshan@gmail.com>
2025-02-15 10:05:04 -0500

    directory path error fixed

--- Week 2/ETL with google cloud/etl_gcs_to_bq.py
+++ Week 2/ETL with google cloud/etl_gcs_to_bq.py
@@ -6,11 +6,11 @@ from prefect_gcp import GcpCredentials
 
 @task(log_prints=True)
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
-    "Download green trip data from GCS"
-    gcs_path = f'data/{color}/{color}_tripdata_{year}-{month:02}'
+    "Download green trip data from GCS - Note that in gcs.get_directory, from path is appended to local path"
+    gcs_path = f'data/{color}/'
     print(f'GCS Path : {gcs_path}')
     gcs_bucket_block = GcsBucket.load("de-zoomcamp-week2")
-    gcs_bucket_block.get_directory(from_path=gcs_path, local_path=f'data/{color}')
+    gcs_bucket_block.get_directory(from_path=gcs_path, local_path=f"./")
     print(f'Returned path : data/{color}/{color}_tripdata_{year}-{month:02}')
     return Path(f'data/{color}/{color}_tripdata_{year}-{month:02}')
 
@@ -26,6 +26,7 @@ def write_bq(df: pd.DataFrame) -> None:
     gcp_credentials_block = GcpCredentials.load("de-zoomcamp-week2")
     
     df.to_gbq(
+        destination_table= 'de-zoomcamp-follow-project.de_zoomcamp_w2.rides',
         project_id='de-zoomcamp-follow-project',
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
